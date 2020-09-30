b9a0da99145134b3fe182eac1ee23485faefaa03
Shadab <shadab19it@gmail.com>
2020-09-30 13:41:12 +0530

    added main page

---
+++ client-web/src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import { RecentActorsTwoTone } from "@material-ui/icons";
+
+import React, { FC } from "react";
+
+const Main: FC = () => {
+  return <div>this is main page</div>;
+};
+
+export default Main;
